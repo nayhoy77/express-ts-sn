595dac6381de5fd6547d4ff7565703e948b1a4a2
nayhoy77้ <flukrattanachot@gmail.com>
2024-09-27 23:12:07 +0700

    first commit

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+// localhost:3000/api/products
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// import * as express from 'express';
+// import * as bodyParser from 'body-parser';
+// import routes from './routes';
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+// // applying the routes to the basepath '/api'
+// app.use('/api', routes);
+// const port = process.env.PORT || 3000;
+// app.get('/', (req, res) => {
+//   res.send('konesaan')
+// })
+// app.listen(port, () => {
+//     console.log(`Listening on http://localhost:${port}`)
+// })
+// // http://localhost:3000/api/products
+const app_1 = __importDefault(require("./app"));
+// get the port from the environment variable or use 3000
+const port = 3000;
+app_1.default.listen(port, () => console.log(`App is listening on http://localhost:${port}`));
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  };
+  
